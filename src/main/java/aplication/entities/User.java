51cbd8ae700ac67d1e32a298afd446102439e376
RomeuRocha <romeu.d.rocha@gmail.com>
2020-08-10 15:40:45 -0300

    lombok removido, para testes

--- src/main/java/aplication/entities/User.java
+++ src/main/java/aplication/entities/User.java
@@ -15,8 +15,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@NoArgsConstructor @AllArgsConstructor @ToString(of = "name") @EqualsAndHashCode(of = "id")
-@Getter @Setter
 public class User implements Serializable{
 	
 	/**
@@ -29,5 +27,23 @@ public class User implements Serializable{
 	private Long id;
 	
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
